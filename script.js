df886b2f3db0ecb01105f959d49a1337440b7bac
wafflas <giannikakisgiwrgos@gmail.com>
2024-04-11 22:02:59 +0300

    new file:   about.html 	new file:   fournos.html 	new file:   images/bakery_logo-removebg-preview.png 	new file:   images/bakery_logo.png 	new file:   images/bakery_logo_remove.png 	new file:   images/bakery_logotrans.png 	new file:   images/giagia.jpg 	new file:   images/gluka.jpg 	new file:   images/gluka2.jpg 	new file:   images/isxsnika.jpg 	new file:   images/kafes1.jpg 	new file:   images/kafes2.jpg 	new file:   images/kalathi.jpg 	new file:   images/katasthma1.jpg 	new file:   images/katasthma2.jpg 	new file:   images/mizes.jpg 	new file:   images/mizes2.jpg 	new file:   images/pswmi1.jpg 	new file:   images/pswmi1slide.jpg 	new file:   images/pswmi2.jpg 	new file:   images/pswmi3.jpg 	new file:   images/pswmi4.jpg 	new file:   images/pswmiaagkalia.jpg 	new file:   images/sandwich.jpg 	new file:   images/sandwich2.jpg 	new file:   images/tampela.jpg 	new file:   images/tapsi.jpg 	new file:   images/topswmi.jpg 	new file:   images/tzenh.jpg 	new file:   images/zumwma.jpg 	new file:   images/zumwma2.jpg 	new file:   index.html 	new file:   katasthmata.html 	new file:   node_modules/.package-lock.json 	new file:   node_modules/is-dom-node-list/CHANGELOG.md 	new file:   node_modules/is-dom-node-list/LICENSE 	new file:   node_modules/is-dom-node-list/README.md 	new file:   node_modules/is-dom-node-list/dist/is-dom-node-list.es.js 	new file:   node_modules/is-dom-node-list/dist/is-dom-node-list.js 	new file:   node_modules/is-dom-node-list/dist/is-dom-node-list.min.js 	new file:   node_modules/is-dom-node-list/package.json 	new file:   node_modules/is-dom-node/CHANGELOG.md 	new file:   node_modules/is-dom-node/LICENSE 	new file:   node_modules/is-dom-node/README.md 	new file:   node_modules/is-dom-node/dist/is-dom-node.es.js 	new file:   node_modules/is-dom-node/dist/is-dom-node.js 	new file:   node_modules/is-dom-node/dist/is-dom-node.min.js 	new file:   node_modules/is-dom-node/package.json 	new file:   node_modules/miniraf/CHANGELOG.md 	new file:   node_modules/miniraf/LICENSE 	new file:   node_modules/miniraf/README.md 	new file:   node_modules/miniraf/dist/miniraf.es.js 	new file:   node_modules/miniraf/dist/miniraf.js 	new file:   node_modules/miniraf/dist/miniraf.min.js 	new file:   node_modules/miniraf/package.json 	new file:   node_modules/rematrix/CHANGELOG.md 	new file:   node_modules/rematrix/LICENSE 	new file:   node_modules/rematrix/README.md 	new file:   node_modules/rematrix/dist/rematrix.es.js 	new file:   node_modules/rematrix/dist/rematrix.js 	new file:   node_modules/rematrix/dist/rematrix.min.js 	new file:   node_modules/rematrix/package.json 	new file:   node_modules/scrollreveal/CHANGELOG.md 	new file:   node_modules/scrollreveal/README.md 	new file:   node_modules/scrollreveal/dist/scrollreveal.es.js 	new file:   node_modules/scrollreveal/dist/scrollreveal.js 	new file:   node_modules/scrollreveal/package.json 	new file:   node_modules/tealight/CHANGELOG.md 	new file:   node_modules/tealight/LICENSE 	new file:   node_modules/tealight/README.md 	new file:   node_modules/tealight/dist/tealight.es.js 	new file:   node_modules/tealight/dist/tealight.js 	new file:   node_modules/tealight/dist/tealight.min.js 	new file:   node_modules/tealight/package.json 	new file:   node_modules/tealight/src/index.d.ts 	new file:   proionta.html 	new file:   script.js 	new file:   scrollreveal.min.js 	new file:   style.css 	new file:   symbols/chevron-svgrepo-com.svg 	new file:   symbols/close_FILL0_wght400_GRAD0_opsz48.svg 	new file:   symbols/five_star_rating.svg 	new file:   symbols/icons8-facebook.svg 	new file:   symbols/icons8-instagram.svg 	new file:   symbols/icons8-search.svg 	new file:   symbols/mail-alt-3-svgrepo-com.svg 	new file:   symbols/map-location-pin-svgrepo-com.svg 	new file:   symbols/menu_FILL0_wght400_GRAD0_opsz48.svg

---
+++ script.js
@@ -0,0 +1,98 @@
+
+//krupse mpara
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
+
+
+
+
+
+
+
+// slideshow
+var slideIndex = 1;
+var timer = null;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  clearTimeout(timer);
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  clearTimeout(timer);
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  timer = setTimeout(function() { plusSlides(1); }, 8000); // Αυτόματη προχώρηση σε επόμενη φωτογραφία κάθε 8 δευτερόλεπτα
+} 
+
+
+
+/* ----reviews---- */
+
+var slideIndexRev = 1;
+var timerRev = null;
+showSlidesRev(slideIndexRev);
+
+function plusSlidesRev(n) {
+  showSlidesRev(slideIndexRev += n);
+}
+
+function currentSlideRev(n) {
+  showSlidesRev(slideIndexRev = n);
+}
+
+function showSlidesRev(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides-reviews");
+  var dots = document.getElementsByClassName("dot-reviews");
+  if (n > slides.length) {slideIndexRev = 1}
+    if (n < 1) {slideIndexRev = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndexRev-1].style.display = "block";
+  dots[slideIndexRev-1].className += " active";
+  timerRev = setTimeout(function() { plusSlidesRev(1); }, 8000); // Αυτόματη προχώρηση σε επόμενη φωτογραφία κάθε 8 δευτερόλεπτα
+} 
+
+
+ 
+
+document.addEventListener("DOMContentLoaded", function() {
+  var logoPopup = document.querySelector(".logo-popup")
+  var slides = document.querySelectorAll(".mySlides")
+  slides.forEach(function(slide) {
+      logoPopup.style.display = "block"
+  })
+})
+
+
+
+
+
+
